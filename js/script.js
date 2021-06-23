af0f71d04bebe445ab89de8b9e8ca005c9eb7892
Piero <piero.onorati@virgilio.it>
2021-06-23 16:46:43 +0200

    add style

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            toDoList: [
+                'Studiare Vue.js',
+                'Terminare progetto "To-Do List" ',
+                'Andare in palestra'
+            ]
+
+        }
+    }
+);
