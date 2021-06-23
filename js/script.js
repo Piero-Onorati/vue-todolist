605728e9f86520808dbc3f496b02255469b94de6
Piero <piero.onorati@virgilio.it>
2021-06-23 17:06:52 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 const app = new Vue (
     {
         el: '#app',
+
         data: {
             toDoList: [
                 'Studiare Vue.js',
                 'Terminare progetto "To-Do List" ',
                 'Andare in palestra'
-            ]
+            ],
+            newToDo: ''
+
+        },
+
+        methods: {
+            addNewTask : function(){
+                if (this.newToDo !=''){
+                    this.toDoList.push(this.newToDo)
+                }
+                
+            },
+
+            removeTask : function(index){
+                this.toDoList.splice(index,1)
+            }
 
         }
     }
